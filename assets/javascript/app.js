501c802f6777d2a5284d926aec25d97db97ef814
Devin Collier <devinlcollier@gmail.com>
2017-12-16 13:59:42 -0700

    fixed bug in SplayButton

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,52 @@
 // On click event when submit button is clicked
 $("#submitBtn").on("click", function() {
-  event.preventDefault();
+    event.preventDefault();
 
-  // Variables for artist and song entered
-  var artistName = $("#artistInput").val().trim();
-  var songTitle = $("#songInput").val().trim();
+    // Variables for artist and song entered
+    var artistName = $("#artistInput").val().trim();
+    var songTitle = $("#songInput").val().trim();
 
-  // Variable to store object
-  var newArtistSong = {
-    artist: artistName,
-    song: songTitle
-  };
+    // Variable to store object
+    var newArtistSong = {
+        artist: artistName,
+        song: songTitle
+    };
 
-  // URL construct to search lyrics.ovh for artist and song
-  var queryURL = "https://api.lyrics.ovh/v1/" + newArtistSong.artist + "/" + newArtistSong.song;
+    // URL construct to search lyrics.ovh for artist and song
+    var queryURL = "https://api.lyrics.ovh/v1/" + newArtistSong.artist + "/" + newArtistSong.song;
 
-  // AJAX GET request
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
+    // AJAX GET request
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) { // When data returns from API
+        console.log(response);
 
-  // When data returns from API
-  .done(function(response) {
-      console.log(response);
+        // var results = response.data;
+        console.log(response.lyrics);
 
-    // var results = response.data;
-      console.log(response.lyrics);
+        // Replace \n with <br>
+        // var str = response.lyrics;
+        // var re = "\n";
+        // var newstr = str.replace(re, "<br>");
+        // console.log(newstr);
 
-    // Replace \n with <br>
-    // var str = response.lyrics;
-    // var re = "\n";
-    // var newstr = str.replace(re, "<br>");
-    // console.log(newstr);
+        // Append to lyrics section
+        $("#lyricsSection").append(response.lyrics);
+    });
 
-    // Append to lyrics section
-    $("#lyricsSection").append(response.lyrics);
+    // Clear lyrics
+    $("#lyricsSection").empty();
 
-  });
+    // Clear form
+    $("#artistInput").val("");
+    $("#songInput").val("");
 
-  // Clear lyrics
-  $("#lyricsSection").empty();
+    console.log(newArtistSong.artist);
+    console.log(newArtistSong.song);
 
-  // Clear form
-  $("#artistInput").val("");
-  $("#songInput").val("");
-
-  console.log(newArtistSong.artist);
-  console.log(newArtistSong.song);
-
-  // Prepend artist and song to table
-  $("#artistSongTable > tbody").prepend("<tr><td>" + newArtistSong.artist + "</td><td>" + newArtistSong.song + "</td></tr>");
-
-
+    // Prepend artist and song to table
+    $("#artistSongTable > tbody").prepend("<tr><td>" + newArtistSong.artist + "</td><td>" + newArtistSong.song + "</td></tr>");
 });
 
 $("#sign-in").on("click", function() {
@@ -90,7 +84,7 @@ function Ssearch(accessToken, query) {
 function SplayButton(uri) {
     if (uri.indexOf("/track/") !== -1) {
         var play = $("<iframe>");
-        play.attr("src", "https://open.spotify.com/embed?uri=spotify:track:" + uri.slice(uri.lastIndexOf("/")));
+        play.attr("src", "https://open.spotify.com/embed?uri=spotify:track:" + uri.slice(uri.lastIndexOf("/") + 1));
         play.attr("width", "300");
         play.attr("height", "380");
         play.attr("frameborder", "0");
