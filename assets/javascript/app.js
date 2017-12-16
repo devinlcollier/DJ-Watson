83bbac3f9dfc9b01e30143e0edee4d4f745b4a31
Pauletta Riggs <riggs20@gmail.com>
2017-12-16 11:42:43 -0700

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,16 +12,6 @@ $("#submitBtn").on("click", function() {
     song: songTitle
   };
 
-  // Clear form
-  $("#artistInput").val("");
-  $("#songInput").val("");
-
-  console.log(newArtistSong.artist);
-  console.log(newArtistSong.song);
-
-  // Prepend artist and song to table
-  $("#artistSongTable > tbody").prepend("<tr><td>" + newArtistSong.artist + "</td><td>" + newArtistSong.song + "</td></tr>");
-
   // URL construct to search lyrics.ovh for artist and song
   var queryURL = "https://api.lyrics.ovh/v1/" + newArtistSong.artist + "/" + newArtistSong.song;
 
@@ -35,15 +25,34 @@ $("#submitBtn").on("click", function() {
   .done(function(response) {
       console.log(response);
 
-    // Store array of results
     // var results = response.data;
       console.log(response.lyrics);
 
+    // Replace \n with <br>
+    // var str = response.lyrics;
+    // var re = "\n";
+    // var newstr = str.replace(re, "<br>");
+    // console.log(newstr);
+
     // Append to lyrics section
     $("#lyricsSection").append(response.lyrics);
-      
-    
-  });     
+
+  });
+
+  // Clear lyrics
+  $("#lyricsSection").empty();
+
+  // Clear form
+  $("#artistInput").val("");
+  $("#songInput").val("");
+
+  console.log(newArtistSong.artist);
+  console.log(newArtistSong.song);
+
+  // Prepend artist and song to table
+  $("#artistSongTable > tbody").prepend("<tr><td>" + newArtistSong.artist + "</td><td>" + newArtistSong.song + "</td></tr>");
+
+
 });
 
 
